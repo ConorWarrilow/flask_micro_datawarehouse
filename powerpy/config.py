72f168fe838be66431f6332a9fd039e5c1794b5f
ConorWarrilow <152389538+ConorWarrilow@users.noreply.github.com>
2024-10-13 21:15:15 +0800

    Update config.py

--- powerpy/config.py
+++ powerpy/config.py
@@ -1,13 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = 'ee70bd9301abb031758830dce49fe3c8'
+    SECRET_KEY = 'YOUR_KEY'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    #MAIL_USERNAME = 'conorwarrilow@gmail.com'
-    #MAIL_PASSWORD = 'clbp riov tvyr ayqv'
     MAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
